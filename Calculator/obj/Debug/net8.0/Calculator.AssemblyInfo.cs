82dc8a7aafefc1a7ed4899359dc6dd0f0089dbc3
Matheus Lima <matheusslimabm@gmail.com>
2024-11-14 21:31:29 -0300

    saida

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97fcfbd6bc0835fb93521d771fa6fd7e9510e7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e348c532a7b4eb2f3ade46c4af05974d185ea453")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
