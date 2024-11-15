e348c532a7b4eb2f3ade46c4af05974d185ea453
Matheus Lima <matheusslimabm@gmail.com>
2024-11-14 21:28:10 -0300

    Criação do menu no console

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,10 +6,31 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            //Soma();
-            //Subtracao();
-            //Divisao();
-            Multiplicacao();
+            Menu();
+        }
+
+        static void Menu() {
+            Console.Clear();
+
+            Console.WriteLine("O que deseja fazer? ");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2- Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Selecione uma opção: ");
+
+            short res = short.Parse( Console.ReadLine() + "");
+
+            switch(res) {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                default: Menu(); break;
+
+            }
         }
 
         static void Soma() 
@@ -28,6 +49,7 @@ namespace Calculator
 
             Console.WriteLine($"O resultado da soma é {resultado}");
             Console.ReadKey();
+            Menu();
 
         }
 
@@ -46,6 +68,7 @@ namespace Calculator
             float resultado = v1 - v2;
             Console.WriteLine($"O resultado da subtração é  {resultado}");
             Console.ReadKey();
+            Menu();
         }
 
         static void Divisao() {
@@ -62,6 +85,7 @@ namespace Calculator
             float resultado = v1 / v2;
             Console.WriteLine($"O resultado da divisão é {resultado}");
             Console.ReadKey();
+            Menu();
         }
 
         static void Multiplicacao() {
@@ -78,6 +102,7 @@ namespace Calculator
             float resultado = v1 * v2;
             Console.WriteLine($"O resultado da multiplicação é {resultado}");
             Console.ReadKey();
+            Menu();
         }
     }
 }
--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+469e879b5a1cbb5cea012c872851ed20fc078423")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97fcfbd6bc0835fb93521d771fa6fd7e9510e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
