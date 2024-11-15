e97fcfbd6bc0835fb93521d771fa6fd7e9510e7d
Matheus Lima <matheusslimabm@gmail.com>
2024-11-14 21:13:49 -0300

    Funções de operação

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,19 +6,78 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            //Soma();
+            //Subtracao();
+            //Divisao();
+            Multiplicacao();
+        }
+
+        static void Soma() 
+        {
             Console.Clear();
 
             Console.WriteLine("Primeiro valor: ");
-            float v1 = float.Parse(Console.ReadLine());
+            float v1 = float.Parse(Console.ReadLine() + "");
 
             Console.WriteLine("Segundo valor: ");
-            float v2 = float.Parse(Console.ReadLine());
+            float v2 = float.Parse(Console.ReadLine() + "");
 
             Console.WriteLine(" ");
 
             float resultado = v1 + v2;
 
             Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.ReadKey();
+
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine() + "");
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine() + "");
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é  {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Divisao() {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine() + "");
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine() + "");
+
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Multiplicacao() {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine() + "");
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine() + "");
+
+            Console.WriteLine("");
+
+            float resultado = v1 * v2;
+            Console.WriteLine($"O resultado da multiplicação é {resultado}");
+            Console.ReadKey();
         }
     }
 }
--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+504efcd70fb4a172d97f011beab24ad193c6fabe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+469e879b5a1cbb5cea012c872851ed20fc078423")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
